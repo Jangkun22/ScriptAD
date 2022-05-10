ef13b5970a2d849e19d006ced04905cdaf9feabd
Jangkun22 <mg_22@hotmail.fr>
2022-05-10 18:48:40 +0200

    Update Script_Save_User.ps1
    Test-Path BackUp Folders + Crea if doesn't exist.

--- Script_Save_User.ps1
+++ Script_Save_User.ps1
@@ -12,7 +12,29 @@ $DirBackUp = '\\SRVFRANCISCA\Sauvegardes'
 $DirFiles = 'C:\Users'
 $DirLog = 'C:\Users\Administrateur\documents\log.txt'
 $Computer = HOSTNAME.EXE
+
+# Test du chemin d'accès du dossier de sauvegarde du poste de l'utilisateur.
+$TestDirUserComputer = Test-Path -Path $DirBackUp\$env:USERNAME\$Computer
+
+# Test du chemin d'accès du dossier de sauvegarde de l'utilisateur.
+$TestDirSaveUser = Test-Path -Path $DirBackUp\$env:USERNAME
+
+# Création d'un dossier de sauvegarde du poste de l'utilisateur s'il n'existe pas et création d'un dossier de sauvegarde de l'utilisateur et de son poste si le dossier de sauvegarde de l'utilisateur n'existe pas.
+switch ($TestDirUserComputer) {
+    False { 
+        switch ($TestDirSaveUser) {
+            False { 
+                New-Item -Name $env:USERNAME -ItemType Directory -Path $DirBackUp | Out-Null
+                New-Item -Name $Computer -ItemType Directory -Path $DirBackUp\$env:USERNAME | Out-Null
+                }
+            True {New-Item -Name $Computer -ItemType Directory -Path $DirBackUp\$env:USERNAME | Out-Null}
+        } 
+    }
+    Default {
+        # Compression des données à sauvegarder sur dans le dossier de sauvegarde de l'utilisateur de chacun des PC.
+        Compress-Archive -Path $DirFiles -Destination $DirBackUp\$env:USERNAME\$Computer >$DirLog
+    }
+}
  
-# Compression des données à sauvegarder sur dans le dossier de sauvegarde de l'utilisateur de chacun des PC.
- Compress-Archive -Path $DirFiles -Destination $DirBackUp\$env:USERNAME\$Computer >$DirLog
+
 
