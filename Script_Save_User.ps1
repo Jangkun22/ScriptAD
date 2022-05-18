b7f1f8b6f793003946144b9c456cfa1ba92672d1
Jangkun22 <mg_22@hotmail.fr>
2022-05-18 08:43:44 +0200

    Update Script_Save_User.ps1

--- Script_Save_User.ps1
+++ Script_Save_User.ps1
@@ -1,17 +1,20 @@
 ﻿<#
 
 Auteur : Martin Guigot
-Date : 09/05/2022
-Version : 1.1
+Date : 17/05/2022
+Version : 1.2
 Description : Script de sauvegarde des données utilisateurs (C:\Users de chaque PC) dans le serveur (E:\Sauvegarde)
 
 #>
 
+
+
 # Définition des variables.
-$DirBackUp = '\\SRVFRANCISCA\E$\Sauvegarde' 
-$DirFiles = 'C:\Users\*'
+$DirBackUp = "\\SRVFRANCISCA\Sauvegarde$\$env:USERNAME" 
+$DirFiles = "C:\Users\$env:USERNAME\*"
+
+New-Item -ItemType Directory -Path $DirBackUp | Out-Null
+
 
 # Compression des données à sauvegarder dans le dossier de sauvegarde dans une archive au nom du poste. 
 Compress-Archive -Path $DirFiles -Destination $DirBackUp\$env:COMPUTERNAME -force
-
-
